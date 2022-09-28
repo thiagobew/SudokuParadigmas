dc3dddbacd29d1662e0669bc4c274ebd922011d3
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-09-28 14:50:43 -0300

    vergleichssudoku but not working

--- sudokuPython/main.py
+++ sudokuPython/main.py
@@ -1,50 +1,88 @@
 # N is the size of the 2D matrix   N*N
-N = 9
- 
+N = 4
+nSqrt = 2
 # A utility function to print grid
+
+
 def printing(arr):
     for i in range(N):
         for j in range(N):
-            print(arr[i][j], end = " ")
+            print(arr[i][j], end=" ")
         print()
- 
+
+def compare(grid, comparator, case, row, col):
+  if comparator == ">":
+    if case == 0:
+      if grid[row][col] > grid[row-1][col]:
+        return True
+    elif case == 1:
+      if grid[row][col] > grid[row][col+1]:
+        return True
+    elif case == 2:
+      if grid[row][col] > grid[row+1][col]:
+        return True
+    elif case == 3:
+      if grid[row][col] > grid[row][col-1]:
+        return True
+  else:
+    if case == 0:
+      if grid[row][col] < grid[row-1][col]:
+        return True
+    elif case == 1:
+      if grid[row][col] < grid[row][col+1]:
+        return True
+    elif case == 2:
+      if grid[row][col] < grid[row+1][col]:
+        return True
+    elif case == 3:
+      if grid[row][col] < grid[row][col-1]:
+        return True
+  return False
+
 # Checks whether it will be
 # legal to assign num to the
 # given row, col
-def isSafe(grid, row, col, num):
-   
+def isSafe(grid, row, col, num, comparatorsGrid):
+
+    # Checking for comparators
+    comparators = comparatorsGrid[row][col]
+    for i in range(4):
+      if comparators[i] != "":
+        if not compare(grid, comparators[i], i, row, col):
+          return False
+    
     # Check if we find the same num
     # in the similar row , we
     # return false
-    for x in range(9):
-        if grid[row][x] == num:
-            return False
- 
     # Check if we find the same num in
     # the similar column , we
     # return false
-    for x in range(9):
+    for x in range(N):
+        if grid[row][x] == num:
+            return False
         if grid[x][col] == num:
             return False
- 
+
     # Check if we find the same num in
     # the particular 3*3 matrix,
     # we return false
-    startRow = row - row % 3
-    startCol = col - col % 3
-    for i in range(3):
-        for j in range(3):
+    startRow = row - row % nSqrt
+    startCol = col - col % nSqrt
+    for i in range(nSqrt):
+        for j in range(nSqrt):
             if grid[i + startRow][j + startCol] == num:
                 return False
     return True
- 
+
 # Takes a partially filled-in grid and attempts
 # to assign values to all unassigned locations in
 # such a way to meet the requirements for
 # Sudoku solution (non-duplication across rows,
 # columns, and boxes) */
-def solveSudoku(grid, row, col):
-   
+
+
+def solveSudoku(comparatorsGrid, grid, row, col):
+
     # Check if we have reached the 8th
     # row and 9th column (0
     # indexed matrix) , we are
@@ -52,27 +90,27 @@ def solveSudoku(grid, row, col):
     # further backtracking
     if (row == N - 1 and col == N):
         return True
-       
+
     # Check if column value  becomes 9 ,
     # we move to next row and
     # column start from 0
     if col == N:
         row += 1
         col = 0
- 
+
     # Check if the current position of
     # the grid already contains
     # value >0, we iterate for next column
     if grid[row][col] > 0:
-        return solveSudoku(grid, row, col + 1)
-    for num in range(1, N + 1, 1):
-       
+        return solveSudoku(comparatorsGrid, grid, row, col + 1)
+
+    for num in range(1, N + 1):
         # Check if it is safe to place
         # the num (1-9)  in the
         # given row ,col  ->we
         # move to next column
-        if isSafe(grid, row, col, num):
-           
+        if isSafe(grid, row, col, num, comparatorsGrid):
+
             # Assigning the num in
             # the current (row,col)
             # position of the grid
@@ -80,12 +118,12 @@ def solveSudoku(grid, row, col):
             # num in the position
             # is correct
             grid[row][col] = num
- 
+
             # Checking for next possibility with next
             # column
-            if solveSudoku(grid, row, col + 1):
+            if solveSudoku(comparatorsGrid, grid, row, col + 1):
                 return True
- 
+
         # Removing the assigned num ,
         # since our assumption
         # was wrong , and we go for
@@ -93,23 +131,24 @@ def solveSudoku(grid, row, col):
         # diff num value
         grid[row][col] = 0
     return False
- 
+
 # Driver Code
- 
+
+
 # 0 means unassigned cells
-grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
- 
-if (solveSudoku(grid, 0, 0)):
+grid = [0] * N
+for i in range(N):
+    grid[i] = [0] * N
+
+# comparators in clockwise order
+comparatorsGridSize4 = [[["", "<", "<", ""], ["", "", "<", ">"], ["", ">", ">", ""], ["", "", ">", "<"]], 
+                        [[">", ">", "", ""], [">", "", "", "<"], ["<", "<", "", ""], ["<", "", "", ">"]], 
+                        [["", "<", ">", ""], ["", "", ">", ">"], ["", ">", "<", ""], ["", "", "<", "<"]], 
+                        [["<", ">", "", ""], ["<", "", "", "<"], [">", "<", "", ""], [">", "", "", ">"]]]
+
+if (solveSudoku(comparatorsGridSize4, grid, 0, 0)):
     printing(grid)
 else:
     print("no solution  exists ")
- 
+
     # This code is contributed by sudhanshgupta2019a
