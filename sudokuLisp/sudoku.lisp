38b9a77fdb490290efe5c6d1d2d3464277b0e55a
Rafael Vargas <rafael.s.vargas18@gmail.com>
2022-11-10 09:58:49 -0300

    Adding comparations

--- sudokuLisp/sudoku.lisp
+++ sudokuLisp/sudoku.lisp
@@ -1,2 +1,31 @@
 (load "utils.lisp")
 (load "config.lisp")
+
+(defun compareBigger (grid row column value type)
+  (cond 
+      ((= type 0) (or (> value (getXY grid (- row 1) column)) (= (getXY grid (- 1 row) column) 0)))
+      ((= type 1) (or (> value (getXY grid row (+ column 1))) (= (getXY grid row (+ column 1)) 0)))
+      ((= type 2) (or (> value (getXY grid (+ row 1) column)) (= (getXY grid (+ row 1) column) 0)))
+      ((= type 3) (or (> value (getXY grid row (- column 1))) (= (getXY grid row (- column 1)) 0)))
+      (t ())
+  )
+)
+
+(defun compareSmaller (grid row column value type)
+  (cond 
+      ((= type 0) (or (< value (getXY grid (- row 1) column)) (= (getXY grid (- 1 row) column) 0)))
+      ((= type 1) (or (< value (getXY grid row (+ column 1))) (= (getXY grid row (+ column 1)) 0)))
+      ((= type 2) (or (< value (getXY grid (+ row 1) column)) (= (getXY grid (+ row 1) column) 0)))
+      ((= type 3) (or (< value (getXY grid row (- column 1))) (= (getXY grid row (- column 1)) 0)))
+      (t ())
+  )
+)
+
+(defun executeComparison (grid row column value comparator type)
+  (cond 
+      ((= comparator #\.) (true))
+      ((= comparator #\>) (compareBigger grid row column value type))
+      ((= comparator #\<) (compareSmaller grid row column value type))
+      (t ())
+  )
+)
