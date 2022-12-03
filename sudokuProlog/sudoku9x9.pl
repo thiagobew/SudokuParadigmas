a2a2544b83e05ff423c7e8a913b30f2efdfb4050
thiago <thiagoaugustobewiahn@gmail.com>
2022-12-03 18:06:54 -0300

    Solution done, but taking too long

--- sudokuProlog/sudoku9x9.pl
+++ sudokuProlog/sudoku9x9.pl
@@ -34,57 +34,6 @@ smallerSmaller(A, B, C) :-
   smaller(A, B),
   smaller(B, C).
 
-smallerSmallerSmaller(A, B, C, D, E, F) :-
-  smaller(A, B),
-  smaller(C, D),
-  smaller(E, F).
-
-smallerSmallerBigger(A, B, C, D, E, F) :-
-  smaller(A, B),
-  smaller(C, D),
-  bigger(E, F).
-
-menorMaiorMaior(A, B, C, D, E, F) :-
-  smaller(A, B),
-  bigger(C, D),
-  bigger(E, F).
-
-maiorMaiorMaior(A, B, C, D, E, F) :-
-  bigger(A, B),
-  bigger(C, D),
-  bigger(E, F).
-
-maiorMaiorMenor(A, B, C, D, E, F) :-
-  bigger(A, B),
-  bigger(C, D),
-  smaller(E, F).
-
-maiorMenorMenor(A, B, C, D, E, F) :-
-  bigger(A, B),
-  smaller(C, D),
-  smaller(E, F).
-
-maiorMenorMaior(A, B, C, D, E, F) :-
-  bigger(A, B),
-  smaller(C, D),
-  bigger(E, F).
-
-menorMaiorMenor(A, B, C, D, E, F) :-
-  smaller(A, B),
-  bigger(C, D),
-  smaller(E, F).
-
-Solution=[
-  [A1, A2, A3, A4, A5, A6, A7, A8, A9], 
-  [B1, B2, B3, B4, B5, B6, B7, B8, B9], 
-  [C1, C2, C3, C4, C5, C6, C7, C8, C9], 
-  [D1, D2, D3, D4, D5, D6, D7, D8, D9], 
-  [E1, E2, E3, E4, E5, E6, E7, E8, E9], 
-  [F1, F2, F3, F4, F5, F6, F7, F8, F9],
-  [G1, G2, G3, G4, G5, G6, G7, G8, G9], 
-  [H1, H2, H3, H4, H5, H6, H7, H8, H9], 
-  [I1, I2, I3, I4, I5, I6, I7, I8, I9]].
-
 solve(Solution) :-
   % Problem 11 - 9x9
   Solution = [
@@ -97,13 +46,6 @@ solve(Solution) :-
     [G1, G2, G3, G4, G5, G6, G7, G8, G9], 
     [H1, H2, H3, H4, H5, H6, H7, H8, H9], 
     [I1, I2, I3, I4, I5, I6, I7, I8, I9]],
-  
-  % Rows constraint
-  maplist(all_distinct, Solution),
-
-  % Columns constraint
-  transpose(Solution, Columns),
-  maplist(all_distinct, Columns),
 
   % Solution is presented in blocks (like the ones in sudoku), starting at the top left corner
   % Block 1
@@ -135,15 +77,62 @@ solve(Solution) :-
   validate([A7, A8, A9, B7, B8, B9, C7, C8, C9]),
   
   % Block 4
+  biggerBigger(D1, D2, D3), % line 4
+  biggerSmaller(E1, E2, E3), % line 5
+  smallerBigger(F1, F2, F3), % line 6
+  biggerBigger(D1, E1, F1), % column 1
+  smallerSmaller(D2, E2, F2), % column 2
+  smallerSmaller(D3, E3, F3), % column 3
+  validate([D1, D2, D3, E1, E2, E3, F1, F2, F3]),
 
   % Block 5
-  
-  % Block 6
-  
-  % Block 7
-  
-  % Block 8
-  
-  % Block 9
+  biggerSmaller(D4, D5, D6), % line 4
+  biggerSmaller(E4, E5, E6), % line 5
+  smallerBigger(F4, F5, F6), % line 6
+  biggerSmaller(D4, E4, F4), % column 4
+  biggerSmaller(D5, E5, F5), % column 5
+  biggerBigger(D6, E6, F6), % column 6
+  validate([D4, D5, D6, E4, E5, E6, F4, F5, F6]),
 
-solve(Solution).
+  % Block 6
+  smallerBigger(D7, D8, D9), % line 4
+  biggerBigger(E7, E8, E9), % line 5
+  biggerSmaller(F7, F8, F9), % line 6
+  smallerBigger(D7, E7, F7), % column 7
+  biggerBigger(D8, E8, F8), % column 8
+  smallerBigger(D9, E9, F9), % column 9
+  validate([D7, D8, D9, E7, E8, E9, F7, F8, F9]),
+
+  % Block 7
+  smallerBigger(G1, G2, G3), % line 7
+  biggerSmaller(H1, H2, H3), % line 8
+  smallerBigger(I1, I2, I3), % line 9
+  biggerSmaller(G1, H1, I1), % column 1
+  biggerSmaller(G2, H2, I2), % column 2
+  biggerBigger(G3, H3, I3), % column 3
+  validate([G1, G2, G3, H1, E2, E3, I1, I2, I3]),
+
+  % Block 8
+  biggerBigger(G4, G5, G6), % line 7
+  biggerSmaller(H4, H5, H6), % line 8
+  smallerSmaller(I4, I5, I6), % line 9
+  smallerBigger(G4, H4, I4), % column 4
+  biggerBigger(G5, H5, I5), % column 5
+  biggerSmaller(G6, H6, I6), % column 6
+  validate([G4, G5, G6, H4, E5, E6, I4, I5, I6]),
+
+  % Block 9
+  smallerBigger(G7, G8, G9), % line 7
+  smallerSmaller(H7, H8, H9), % line 8
+  smallerBigger(I7, I8, I9), % line 9
+  smallerBigger(G7, H7, I7), % column 7
+  smallerSmaller(G8, H8, I8), % column 8
+  smallerBigger(G9, H9, I9), % column 9
+  validate([G7, G8, G9, H7, E8, E9, I7, I8, I9]),
+
+  % Rows constraint
+  maplist(all_distinct, Solution),
+
+  % Columns constraint
+  transpose(Solution, Columns),
+  maplist(all_distinct, Columns).
